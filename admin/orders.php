03c591d65e3dd31ff038f92aa7962289e24ce4d8
mkayf <muhammadkaif150@gmail.com>
2025-03-10 00:24:40 +0500

    site.manifest fix

--- admin/orders.php
+++ admin/orders.php
@@ -95,7 +95,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon-16x16.png">
-    <link rel="manifest" href="../config/site.webmanifest">
+    <link rel="manifest" href="<?= ROOT_URL ?>site.webmanifest">
     <!-- BOOTSTRAP LINK CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
